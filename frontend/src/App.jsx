a53404aaf52c071a38b408839b9b2f47ef242229
ck <ck@tyloones.com>
2024-04-25 13:23:49 +0530

    frontend:routes created & Links to login signup and home provided

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,12 +5,16 @@ import {
   Routes,
 } from "react-router-dom";
 import Home from "./screens/Home";
+import SignUp from "./screens/SignUp";
+import LogIn from "./screens/LogIn";
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<Home />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/login" element={<LogIn />}/>
       </Routes>
     </BrowserRouter>
   );
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,22 +1,28 @@
 import React from 'react'
 import { MdHome } from "react-icons/md";
 import { IoMdLogIn, IoMdLock } from "react-icons/io";
+import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
     <div className="flex gap-[1.2em] p-2">
-        <div className='flex gap-1 items-center'>
+        {/* Home */}
+        <Link to="/"  className='flex gap-1 items-center'>
             <MdHome className="text-[1.5em]"/>
             <p className='text-xl text-slate-700 hover:text-slate-900 hover:underline'>Home</p>
-        </div>
-        <div className='flex gap-1 items-center'>
+        </Link>
+
+        {/* Login */}
+        <Link to="/login" className='flex gap-1 items-center'>
         <IoMdLogIn className="text-[1.5em]"/>
             <p className='text-xl text-slate-700 hover:text-slate-900 hover:underline'>Login</p>
-        </div>
-        <div className='flex gap-1 items-center'>
+        </Link>
+
+        {/* Signup */}
+        <Link to="/signup" className='flex gap-1 items-center'>
         <IoMdLock className="text-[1.5em]"/>
             <p className='text-xl text-slate-700 hover:text-slate-900 hover:underline'>Signup</p>
-        </div>
+        </Link>
         
     </div>
   )
