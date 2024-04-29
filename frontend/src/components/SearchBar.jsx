bbc6ebbd226952d4e208acb391dcf8dfcbbcc713
ck <ck@tyloones.com>
2024-04-29 13:11:52 +0530

    frontend: searchbar component modified

--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -3,14 +3,14 @@ import { FiSearch } from "react-icons/fi";
 
 const SearchBar = () => {
   return (
-    <div className='relative'>
+    <div className='flex'>
         <input 
             type="search" 
             placeholder='Type city'
-            className="p-2 rounded-lg border border-slate-300 focus:border-slate-500 focus:outline-none"
+            className="p-2 rounded-tl-lg rounded-bl-lg border border-slate-300 focus:border-slate-500 focus:outline-none"
         />
-        <button className="hover:bg-blue-500 p-[0.2em] absolute right-[0.24em] top-[0.24em] rounded-lg">
-            <FiSearch className="text-[1.7em] hover:text-white text-blue-500" />
+        <button className="bg-blue-500 hover:bg-blue-600 p-[0.2em] rounded-tr-lg rounded-br-lg">
+            <FiSearch className="text-[1.7em] text-white" />
         </button>
     </div>
   )
